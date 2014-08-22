c52c938bac8c2db477899b12a82eb25e60c54afe
Daniel <daniel.fung122@yahoo.com>
2014-08-22 16:56:22 -0400

    changes

--- src/main/java/handlerSocketMain.java
+++ src/main/java/handlerSocketMain.java
@@ -24,7 +24,6 @@ public class handlerSocketMain {
 		IndexSession readIndexSession = hsclient.openIndexSession(db, table, "Primary", columns);
 		IndexSession deleteIndexSession = hsclient.openIndexSession(db, table, "Primary", columns);
 		IndexSession updateIndexSession = hsclient.openIndexSession(db, table, "Primary", columns);
-		
 		hsclient.shutdown();
 	}
 	
