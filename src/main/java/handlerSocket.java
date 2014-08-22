880172cc8ab080b81f369a724ae9967ac0d8dcc6
Daniel <daniel.fung122@yahoo.com>
2014-08-22 15:43:42 -0400

    adding files

---
+++ src/main/java/handlerSocket.java
@@ -0,0 +1,134 @@
+package main.java;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+import com.google.code.hs4j.FindOperator;
+import com.google.code.hs4j.IndexSession;
+import com.google.code.hs4j.exception.HandlerSocketException;
+
+public class handlerSocket {
+	
+	private Map<String, List<String>> myhashmap = new HashMap<String, List<String>>();
+	private List<String> data = new ArrayList<String>();
+	
+	/*
+	 * Insert data into hashmap
+	 * 
+	 * @param key the item to be added into hashmap as key.
+	 * @param array the items you want to be added that is associated with the "key".
+	 */
+	public void addtoHashMap(String key, List<String> array){
+		myhashmap.put(key, array);
+	}
+	
+	/*
+	 * Clear the hashmap(myhashmap)
+	 */
+	public void clearHashMap(){
+		myhashmap.clear();
+	}
+	
+	/*
+	 * Look in hashmap(myhashmap) for specific data
+	 * 
+	 * @param key the item that you want to look for in hashmap.
+	 * @return based on key returns data if found in hashmap.
+	 */
+
+	public List<String> findKey(String key){
+		return myhashmap.get(key);
+	}
+	
+	/*
+	 * Insert data into arraylist(data)
+	 * 
+	 * @param datas the data to be inserted into arraylist.
+	 */
+	public void addToList(String datas){
+		data.add(datas);
+	}
+	
+	/*
+	 * clear the arraylist(data)
+	 */
+	public void clearList(){
+		data.clear();
+	}
+	
+	/*
+	 * Finding data in mysql 
+	 * 
+	 * @param session represents an open index.
+	 * @param fields column names in the table.
+	 * @param keys values to be compared with index columns.
+	 * @throws InterruptedException
+	 * @throws TimeoutException
+	 * @throws HandlerSocketException
+	 */
+	//find and place into hashmap
+	public void findData(IndexSession session, String[] fields, String[] keys) throws SQLException, InterruptedException, TimeoutException, HandlerSocketException{	
+		for(int i = 0; i<keys.length; i++){
+			String key = keys[i];
+			String[] test = {key};
+			ResultSet rs = session.find(test);
+			while(rs.next()){
+				for(int j = 0; j<fields.length; j++){
+					addToList(rs.getString(fields[i]));//add to arraylist
+				}
+			addtoHashMap(key, data);
+			clearList();//clear list
+			}
+		}
+	}
+	
+	public void findDataFilter(IndexSession sesion, String[] fields, String[] keys, String[] filter){
+		
+	}
+	/*
+	 * Inserting data into mysql
+	 * 
+	 * @param session represents an open index.
+	 * @param values String array of values to be inserted into a specific table in mysql.
+	 * @throws InterruptedException
+	 * @throws TimeoutException
+	 * @throws HandlerSocketException
+	 */
+	public void insertData(IndexSession session, String[] values) throws InterruptedException, TimeoutException, HandlerSocketException{
+		session.insert(values);
+	}
+	
+	/*
+	 * Deleting data from mysql
+	 * 
+	 * @param session represents an open index.
+	 * @param values items to be deleted.
+	 * @param operator specifies the comparison operation(EQ, GE, GT, LE, LT) to use.
+	 * @throws InterruptedException
+	 * @throws TimeoutException
+	 * @throws HandlerSocketException
+	 */
+	public void deleteData(IndexSession session, String[] values, FindOperator operator) throws InterruptedException, TimeoutException, HandlerSocketException{
+		session.delete(values, operator);
+	}
+	
+	/*
+	 * Updating data in mysql
+	 * 
+	 * @param session represents an open index.
+	 * @param keys keys that will be compared with index columns.
+	 * @param values values that will be modified.
+	 * @param operator specifies the comparison operation(EQ, GE, GT, LE, LT) to use.
+	 * @throws InterruptedException
+	 * @throws TimeoutException
+	 * @throws HandlerSocketException
+	 */
+	public void updateData(IndexSession session, String[] keys, String[] values, FindOperator operator) throws InterruptedException, TimeoutException, HandlerSocketException{
+		session.update(keys, values, operator);
+	}
+}
---
+++ src/main/java/handlerSocketMain.java
@@ -0,0 +1,31 @@
+package main.java;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.sql.SQLException;
+import java.util.concurrent.TimeoutException;
+
+import com.google.code.hs4j.HSClient;
+import com.google.code.hs4j.exception.HandlerSocketException;
+import com.google.code.hs4j.impl.HSClientImpl;
+import com.google.code.hs4j.IndexSession;
+
+public class handlerSocketMain {
+
+	public static void main(String [] args) throws IOException, InterruptedException, TimeoutException, HandlerSocketException, SQLException{
+		//new client, 9999 is read/write, 9998 read only, 100 connection pools
+		HSClient hsclient = new HSClientImpl(new InetSocketAddress(9999),100);
+		handlerSocket hs = new handlerSocket();
+		
+		String db = "test";//database
+		String table = "test";//table
+		String[] columns = {"test", "test1"};//columns
+		IndexSession writeIndexSession = hsclient.openIndexSession(db, table, "Primary", columns);
+		IndexSession readIndexSession = hsclient.openIndexSession(db, table, "Primary", columns);
+		IndexSession deleteIndexSession = hsclient.openIndexSession(db, table, "Primary", columns);
+		IndexSession updateIndexSession = hsclient.openIndexSession(db, table, "Primary", columns);
+		
+		hsclient.shutdown();
+	}
+	
+}
